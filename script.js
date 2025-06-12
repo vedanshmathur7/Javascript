66eb53f49b1408f57ae74139b4d70347be5c78a7
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-06-13 00:48:43 +0530

    performing operations

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("rajesh ki ghanti");
+let a = 56;
+let b = 100;
+
+console.log("a = ",a,", b = ", b);
+console.log("a + b = ", a+b);
+console.log("a - b = ", a-b);
+console.log("a * b = ", a*b);
+console.log("a / b = ", a/b);
+console.log("a % b = ", a%b);
+console.log("a ** b = ", a**b);
