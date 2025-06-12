e3330f15fcdaabbbb8cd3572d99718d420129da3
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-06-12 23:43:05 +0530

    ,

--- first.js
+++ first.js
@@ -1,3 +1,36 @@
-console.log("Vedansh mathur");
-console.log("vedanhs is prio");
-alert ("champak chacha ");
+// console.log("Vedansh mathur");
+// console.log("vedanhs is prio");
+// alert ("champak chacha ");
+
+// name = "Vedansh Mathur";
+// console.log(name);
+// x  = null;
+// y = undefined;
+// isfollow = false;
+// console.log(x);
+// console.log(y);
+// console.log(isfollow);
+
+
+// var age = 25;
+// var age = 25;
+// var age = 25; // can be updated and re-declared
+
+// let age = 25;
+// age = 67; // can be updated but not re-declare
+
+// const age = 56;
+// const age = 87;
+// age = 78; // neither be updated nor re-declared
+// console.log(age);    
+
+// age = 56;
+// console.log(age); 
+// chappal = 100.56;
+// console.log(chappal);
+// let x; // undefined
+// let y = null; // object
+// y = Symbol("vedansh"); // symbol
+// x = BigInt (125); // 125n //BigInt
+
+
