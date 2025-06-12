ff7ba25aba084669e8799f027122a9a8f9d7cb2b
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-06-13 01:11:02 +0530

    functions

--- script.js
+++ script.js
@@ -2,10 +2,36 @@ console.log("rajesh ki ghanti");
 let a = 56;
 let b = 100;
 
-console.log("a = ",a,", b = ", b);
-console.log("a + b = ", a+b);
-console.log("a - b = ", a-b);
-console.log("a * b = ", a*b);
-console.log("a / b = ", a/b);
-console.log("a % b = ", a%b);
-console.log("a ** b = ", a**b);
+// console.log("a = ",a,", b = ", b);
+// console.log("a + b = ", a+b);
+// console.log("a - b = ", a-b);
+// console.log("a * b = ", a*b);
+// console.log("a / b = ", a/b);
+// console.log("a % b = ", a%b);
+// console.log("a ** b = ", a**b);
+
+// console.log(a == b);
+
+// function toCelsius(fahrenheit) {
+//   return (5/9) * (fahrenheit-32);
+// }
+
+// let value = toCelsius(180);
+
+
+// traditional function creation
+// function greet (name) {
+//     return "hello " + name;
+// }
+
+// console.log(greet("Vedansh")) 
+
+// arrow method
+// const greet = (name) => "hello "+ name;
+// console.log(greet("Naman mathur"))
+
+// const square = (x) => x * x;
+// console.log(square(77));
+
+const hello = () => "Hello bhaiji";
+console.log(hello())
