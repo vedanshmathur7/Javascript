de2e890de07b9edfe7b712f7841b2ff2cf3de3b9
vedanshmathur7 <vedanshmathur007@gmail.com>
2025-08-01 23:26:14 +0530

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,26 @@
+
+
+// for (let i =0; i <= 100; i++){
+//     if (i %2!==0){
+//         console.log(i)
+//     }
+// }
+
+
+
+// let our_num = 100;
+// let user_num = prompt("enter the no.");
+
+
+// while (user_num != our_num){
+//     user_num = prompt("another chance given")
+// }
+
+// console.log("yess guesses it right!")
+
+
+
+// ------------------------qn-----------------------
+
+let username =  prompt("Enter your name");
+console.log(`your username is @${username}${username.length}.`)
