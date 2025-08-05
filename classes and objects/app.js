3500982d0bc570e6eb5379753302b6216ab2980c
vedanshmathur7 <vedanshmathur007@gmail.com>
2025-08-05 23:03:03 +0530

    Create app.js

---
+++ classes and objects/app.js
@@ -0,0 +1,9 @@
+const students = {
+    fullname : "vedansh mathur",
+    email : "vedanshmathur007@gmail.com",
+    marks : 94.4,
+    printMarks : function () {
+        console.log(marks);
+    },
+    
+}
