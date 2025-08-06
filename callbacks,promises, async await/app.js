badae51a6b616045488297a2e3e66669b48576a3
vedanshmathur7 <vedanshmathur007@gmail.com>
2025-08-06 23:17:29 +0530

    Update app.js

--- callbacks,promises, async await/app.js
+++ callbacks,promises, async await/app.js
@@ -95,17 +95,66 @@ function getData (dataId) {
     });
 }
 
-//Promise chaining
-console.log("getting data 1")
-getData(1)
-.then((res) => {
-        console.log("getting data 2")
-        return getData(2);
-    })
-    .then ((res) => {
-        console.log("getting data 3")
-        return getData(3);
-    })
-    .then ((res) => {
-        console.log(res);
+// //Promise chaining
+// console.log("getting data 1")
+// getData(1)
+// .then((res) => {
+//         console.log("getting data 2")
+//         return getData(2);
+//     })
+//     .then ((res) => {
+//         console.log("getting data 3")
+//         return getData(3);
+//     })
+//     .then ((res) => {
+//         console.log(res);
+//     });
+
+
+
+
+// ---------------------async await---------------------
+
+function getData (dataId) {
+    return new Promise ((resolve, reject) => {
+        setTimeout( ()=> {
+            console.log("data", dataId);
+            resolve("success");
+        }, 2000);
     });
+}
+
+
+async function getAllData () {
+    console.log("getting data 1")
+    await getData (1);
+    console.log("getting data 2")
+    await getData (2);
+    console.log("getting data 3")
+    await getData (2);
+    console.log("getting data 4")
+    await getData (3);
+    console.log("getting data 5")
+    await getData (4);
+}
+
+
+
+
+// IIFE : immediately invoked function expression 
+// (called immediately as soon as it is defined)
+
+// (func) ();
+
+(async function() {   //no name of the function
+    console.log("getting data 1")
+    await getData (1);
+    console.log("getting data 2")
+    await getData (2);
+    console.log("getting data 3")
+    await getData (2);
+    console.log("getting data 4")
+    await getData (3);
+    console.log("getting data 5")
+    await getData (4);
+}) ();
